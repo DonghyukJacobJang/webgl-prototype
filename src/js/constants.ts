d20525c4038334fbb4afa9c9c26900c2ff38ecad
Amelie Rosser <amelierosser1986@gmail.com>
2017-12-21 11:44:12 -0500

    Adding path constants for assets

--- src/js/constants.ts
+++ src/js/constants.ts
@@ -6,3 +6,5 @@ export const DEV_STATS = DEV && getQueryFromParams('stats') === null;
 export const DEV_HELPERS = DEV && getQueryFromParams('helpers') === null;
 
 export const BASE_URL = '/';
+export const ASSETS_DIR = `${BASE_URL}assets/`;
+export const WEBGL_DIR = `${ASSETS_DIR}webgl/`;
